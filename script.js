45c509c2c00d86960cf6ff569b892f4f3706ce86
softwareAssasssin <99945568+softwareAssasssin@users.noreply.github.com>
2023-01-23 22:23:32 +0200

    change

---
+++ script.js
@@ -0,0 +1,11 @@
+setInterval(setClock, 1000)
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRation = currentDate.getSeconds() /60
+    const minutesRatio = currentDate.getMinutes()/60
+    const hoursRation = currentDate.getHours()/60
+
+    const secondsRatio = currentDate + currentDate
+    
+}
