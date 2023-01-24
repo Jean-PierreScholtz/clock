12fe2838e18e15b27a4c02800d72bedaa3901729
softwareAssasssin <99945568+softwareAssasssin@users.noreply.github.com>
2023-01-24 10:33:25 +0200

    change

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const secondHand = document.querySelector('[data-second-hand]');
 
 function setClock() {
     const currentDate = new Date()
-    const secondsRation = currentDate.getSeconds() / 60
+    const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRation = (minutesRatio + currentDate.getHours()) / 12
+    
 
-    const secondsRatio = currentDate + currentDate
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRation)
+    setRotation(hourHand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
